60f60202a538dc81982f4be78c04a28b290af9ad
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-06-24 09:21:10 +0300

    Update index.swift

--- index.swift
+++ index.swift
@@ -190,11 +190,15 @@ if levelScores.count == freeLevels {
   print("Game restarted!")
 }
 //-----------------------tuples-----------------
-let credentials = ( "" ,  -1111)
-let fullCredentials = (password: "pass", passcode: 1111)
-
+let credentials = ("", -1111)
 if credentials.0 == "" || credentials.1 < 0 {
-    print("invaild")
-} else{
-    print("the user name is\(credentials.0)")
+  print("Invalid credentials!")
+} else {
+  print("The username is \(credentials.0) and the password is \(credentials.1).")
+}
+let fullCredentials = (password: "pass", passcode: 1111)
+if fullCredentials.password == "" || fullCredentials.passcode < 0 {
+  print("Invalid credentials!")
+} else {
+  print("The password is \(fullCredentials.password) and the passcode is \(fullCredentials.passcode).")
 }
